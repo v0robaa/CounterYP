c5014a3dcf7218fff8e34e4161240a866bb6b3f5
v0robaa <valeriavoroba@gmail.com>
2024-01-14 13:10:47 +0300

    Изменены модификаторы доступа, удалена лишняя функция, отредактирован порядок свойств и функций

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,27 +9,25 @@ class ViewController: UIViewController {
     
     private var counter = 0
     
-    func dateAndTimeFormatter(date: Date)->String{
+    @IBOutlet private weak var counterRebootButton: UIButton!
+    @IBOutlet private weak var decreaseButton: UIButton!
+    @IBOutlet private weak var increaseButton: UIButton!
+    @IBOutlet private weak var counterOutputLabel: UILabel!
+    @IBOutlet private weak var logTextView: UITextView!
+    
+    private func dateAndTimeFormatter(date: Date)->String{
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "dd/MM/yy HH:mm:ss"
         return dateFormatter.string(from: date)
     }
     
-    
-    @IBOutlet weak var counterRebootButton: UIButton!
-    @IBOutlet weak var decreaseButton: UIButton!
-    @IBOutlet weak var increaseButton: UIButton!
-    @IBOutlet weak var counterOutputLabel: UILabel!
-    @IBOutlet weak var logTextView: UITextView!
-    
-    
-    @IBAction func counterRebootButtonDidTap(_ sender: Any) {
+    @IBAction private func counterRebootButtonDidTap(_ sender: Any) {
         counter = 0
         counterOutputLabel.text = "0"
         logTextView.text.append("\n\(dateAndTimeFormatter(date: Date())): значение сброшено")
     }
     
-    @IBAction func decreaseButtonDidTap(_ sender: Any) {
+    @IBAction private func decreaseButtonDidTap(_ sender: Any) {
         if counter > 0{
             counter -= 1
             counterOutputLabel.text = String(counter)
@@ -41,18 +39,11 @@ class ViewController: UIViewController {
     }
     
     
-    @IBAction func increaseButtonDidTap(_ sender: Any) {
+    @IBAction private func increaseButtonDidTap(_ sender: Any) {
         counter += 1
         counterOutputLabel.text = String(counter)
         logTextView.text.append("\n\(dateAndTimeFormatter(date: Date())): значение изменено на +1")
     }
-    
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
 
 }
 
